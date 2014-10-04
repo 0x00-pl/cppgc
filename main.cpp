b7af28fb671f181e3bd7a3222634b24d96e15695
pl <0x00.pl@gmail.com>
2014-10-04 18:04:27 +0800

    gc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <mutex>
+using namespace std;
+#include "src/gc.h"
+
+mutex Mstdout;
+bool glock=false;
+
+void f(int n){
+  int count=0;
+  this_thread::sleep_for(chrono::seconds(1));
+  /*Mstdout.lock();
+  cout<<"beg "<<n<<endl;
+  Mstdout.unlock();*/
+  while(!glock){ count++; 
+    //this_thread::yield(); 
+    this_thread::sleep_for(chrono::milliseconds(10));
+  }
+  Mstdout.lock();
+  cout<<"end "<<n<<" : "<<count<<endl;
+  Mstdout.unlock();
+}
+
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  vector<thread> tp;
+  for(int i=0; i<1000; i++){
+    tp.push_back(thread(f,i));
+  }
+  //this_thread::sleep_for(chrono::seconds(1));
+  glock=true;
+  for(thread& t : tp){
+    t.join();
+  }
+  std::cout << "Hello, world!" << std::endl;
+  return 0;
 }
